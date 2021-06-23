8bc649849dc5e7c40598e6bdc69796048cc3636a
wegiel <wegiel@pop-os.localdomain>
2021-06-23 19:21:25 +0200

    added working endpoints

--- actions/api/taskActions.js
+++ actions/api/taskActions.js
@@ -1,24 +1,53 @@
 const Task = require('../../db/models/task');
 
 class TaskActions {
-    getTasks(req, res){
-        res.send('api działa');
+
+    async getTasks(req, res){
+        const doc = await Task.find({});
+        res.status(200).json(doc);
     }
-    getTask(req, res){
-        res.send('info');
-    }
-    updateTask(req, res){
-        res.send('zaktualizowane');
-    }
-    deleteTask(req, res){
+
+    async getTask(req, res){
         const id = req.params.id;
-        res.send('usunięte, id:' + id);
+        const task = await Task.findOne({_id: id});
+        res.status(200).json(task);
     }
-    saveTask(req, res){
+
+    async updateTask(req, res){
+        const id = req.params.id;
         const title = req.body.title;
         const body = req.body.body;
-        res.send('stworzona. tytuł: ' + title + 'treść: ' + body);
+        
+        const task = await Task.findOne({_id: id});
+        task.title = title;
+        task.body = body;
+
+        res.status(201).json(task);
+    }
+
+    async deleteTask(req, res){
+        const id = req.params.id;
+        await Task.deleteOne({_id: id});
+        res.sendStatus(204);
+    }
+
+    async saveTask(req, res){
+        const title = req.body.title;
+        const body = req.body.body;
+
+        let task;
+
+        try {
+            task = new Task({ title, body });
+            await task.save();
+        } catch (err) {
+            return res.status(422).json({ message: err.message })
+        };
+        
+        
+        res.status(201).json(task);
     } 
+
 };
 
 module.exports = new TaskActions();
--- db/models/task.js
+++ db/models/task.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
-const Task = mongoose.model('task', {
-    title: String,
-    body: String
+const TaskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
 });
+
+const Task = mongoose.model('task', TaskSchema);
 
 module.exports = Task;
